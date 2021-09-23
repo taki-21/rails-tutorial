1cc9426cad242187e0d34d9a825b51924bdee7fd
taki-21 <taki.it.0208.21@gmail.com>
2021-09-23 10:03:20 +0900

    Add: user index

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255}, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
 
-  has_secure_password
-  validates :password, presence: true, length: {minimum: 6}
+  has_secure_password # オブジェクト生成時に存在性を検証
+  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
